b8b183f3b1c11185ac7111f6130b8a35e65a04d3
Charlotte Y <105072512+charlotteY29@users.noreply.github.com>
2022-05-18 21:51:40 +0000

    Finished

--- part3.py
+++ part3.py
@@ -11,4 +11,5 @@ number1 = int(input("Enter a number: "))
 number2 = int(input("Enter another number: "))
 
 print ("The sum of your numbers is", number1 + number2)
-print("Seven times your second number is", 7 * number2)
+print("Seven times your second number is", 7 * number2)1
+
